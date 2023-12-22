020c92f5e5dd9d1bf198f36c6c7db3d9638f2f2a
Daniel Liu <xyfc128@gmail.com>
2023-12-22 16:03:36 +0800

    find bugs script now diff the output  and generate the input that cause impes to match wrong

--- ipmes-rust/scripts/find_bugs.py
+++ ipmes-rust/scripts/find_bugs.py
@@ -76,11 +76,84 @@ def get_num_results_from_stdout(stdout: bytes) -> int:
     
     return int(match_result.group(1))
 
+def get_match_results_from_stderr(stderr: bytes):
+    lines = stderr.decode().split('\n')
+    ans_pattern = re.compile('Pattern Match: \[([0-9,\s]+)\]')
+    match_results = []
+    for line in lines:
+        found = ans_pattern.search(line)
+        if found is None:
+            continue
+        ids = found.group(1).split(', ')
+        match_results.append(ids)
+
+    return match_results
+
+def find_wrong_answers(answers: list[list[str]], match_results: list[list[str]]):
+    ans_dict = {}
+    for ans_ids in answers:
+        ans_dict[','.join(ans_ids)] = 0
+    
+    false_positive = []
+    for match_ids in match_results:
+        key = ','.join(match_ids)
+        if key not in ans_dict:
+            false_positive.append(key)
+            continue
+        ans_dict[key] += 1
+    
+    true_negative = []
+    for key, val in ans_dict.items():
+        if val == 0:
+            true_negative.append(key)
+        elif val > 1:
+            print(f'The match result {key} appears {val} times')
+    
+    return false_positive, true_negative
+
+def reassign_event_id(event_string: str) -> str:
+    lines = event_string.split('\n')
+    output = ''
+    node_id_map = {}
+    edge_id_map = {}
+    for line in lines:
+        fields = line.split(',')
+        if len(fields) < 6:
+            continue
+        
+        edge_id_map.setdefault(fields[3], str(len(edge_id_map) + 1))
+        fields[3] = edge_id_map[fields[3]]
+
+        node_id_map.setdefault(fields[4], str(len(node_id_map) + 1))
+        node_id_map.setdefault(fields[5], str(len(node_id_map) + 1))
+        fields[4] = node_id_map[fields[4]]
+        fields[5] = node_id_map[fields[5]]
+        output += ','.join(fields) + '\n'
+
+    return output
+
+def gen_wrong_answer(input_events, event_ids, out_file, expect_num):
+    print(f'Generating small input graph expecting {expect_num} results to {out_file}')
+    event_list = []
+    for id in event_ids:
+        event_list.append(input_events[id])
+    event_list.sort()
+    event_string = ''.join(x[1] for x in event_list)
+    event_string = reassign_event_id(event_string)
+
+    with open(out_file, 'w') as f:
+        f.write(event_string)
+
 if __name__ == '__main__':
+    import argparse
+    parser = parser = argparse.ArgumentParser(
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                description='Automatic bug finder')
+    
     ans_folder = '../data/answer'
     data_folder = '../data/preprocessed'
     out_dir = '../data/temp'
-    pattern = 'SP8_regex'
+    pattern = 'SP9_regex'
     data = 'attack'
 
     os.makedirs(out_dir, exist_ok=True)
@@ -88,32 +161,28 @@ if __name__ == '__main__':
     ans_file = get_ans_file(ans_folder, pattern, data)
     answers = parse_ans_file(ans_file)
     
+    print('Indexing data graph...')
     input_events = index_data_graph(os.path.join(data_folder, f'{data}.csv'))
+
+    run_args = ['cargo', 'run', '--', f'../data/universal_patterns/{pattern}.json', os.path.join(data_folder, f'{data}.csv')]
+    print(run_args)
+    os.environ['RUST_LOG'] = 'info'
+    run = subprocess.run(run_args, capture_output=True)
+    os.environ['RUST_LOG'] = ''
+
+    match_results = get_match_results_from_stderr(run.stderr)
+    false_positive, true_negative = find_wrong_answers(answers, match_results)
+
+    if len(false_positive) == 0 and len(true_negative) == 0:
+        print('The match result is the same as the answer')
+        exit(0)
+
+    print('Among {} match results, there are {} results are not in answer, and {} answers not found in the results'
+          .format(len(match_results), len(false_positive), len(true_negative)))
     
-    for ans_num, ans_ids in enumerate(answers, start=1):
-        event_list = []
-        for id in ans_ids:
-            event_list.append(input_events[id])
-        event_list.sort()
-        event_string = ''.join(x[1] for x in event_list)
-        
-        with tempfile.NamedTemporaryFile(mode='w+') as fp:
-            fp.write(event_string)
-            fp.close()
-
-            run = subprocess.run(['cargo', 'run', '--', f'../data/universal_patterns/{pattern}.json', os.path.join(data_folder, f'{data}.csv')], capture_output=True)
-        
-        num_result = get_num_results_from_stdout(run.stdout)
-        if num_result != 1:
-            print(f'The number of matches should be 1, but found {num_result} instead')
-            print(f'In {ans_file}:')
-            print(f'The #{ans_num} answer:', ','.join(ans_ids))
-
-            out_file = os.path.join(out_dir, f'expect_one_{pattern}.csv')
-            with open(out_file, 'w') as f:
-                f.write(event_string)
-
-            print(f'The corresponding input has been written to {out_file}')
-            break
-            
-    print('All tests passed')
+    if len(false_positive) > 0:
+        ids = false_positive[0].split(',')
+        gen_wrong_answer(input_events, ids, os.path.join(out_dir, f'expect_no_{pattern}.csv'), 0)
+    else:
+        ids = true_negative[0].split(',')
+        gen_wrong_answer(input_events, ids, os.path.join(out_dir, f'expect_{1}_{pattern}.csv'), 0)
