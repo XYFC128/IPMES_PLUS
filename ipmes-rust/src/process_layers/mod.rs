b7fe144e0a141d18c49a123f18c13fb7882ec0c0
Daniel Liu <xyfc128@gmail.com>
2024-01-24 15:08:59 +0800

    intergrate the new matching layer into the rest of IPMES

--- ipmes-rust/src/process_layers/mod.rs
+++ ipmes-rust/src/process_layers/mod.rs
@@ -1,10 +1,11 @@
-pub mod parse_layer;
 pub mod composition_layer;
 pub mod join_layer;
-pub mod uniqueness_layer;
 pub mod matching_layer;
+pub mod parse_layer;
+pub mod uniqueness_layer;
 
-pub use parse_layer::ParseLayer;
 pub use composition_layer::CompositionLayer;
 pub use join_layer::JoinLayer;
+pub use matching_layer::MatchingLayer;
+pub use parse_layer::ParseLayer;
 pub use uniqueness_layer::UniquenessLayer;
