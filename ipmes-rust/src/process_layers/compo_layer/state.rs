38bb7e3b3fa668bf0935518f37e85413b2d64dda
Daniel Liu <xyfc128@gmail.com>
2024-04-19 16:38:41 +0800

    handle flo a normal event in matching layer

--- ipmes-rust/src/process_layers/compo_layer/state.rs
+++ ipmes-rust/src/process_layers/compo_layer/state.rs
@@ -4,10 +4,8 @@ use thiserror::Error;
 
 #[derive(Clone, Copy)]
 pub enum StateInfo {
-    Normal { next_state: u32 },
+    Default { next_state: u32 },
     Output { subpattern_id: u32 },
-    InitFlow { next_state: u32, agg_state: u32 },
-    AggFlow { next_state: u32 },
     InitFreq { next_state: u32 },
     AggFreq { next_state: u32, frequency: u32 },
 }
@@ -16,16 +14,8 @@ type EntityId = u64;
 
 #[derive(Clone)]
 pub enum StateData {
-    Normal {
+    Default {
         next_state: u32,
-    },
-    InitFlow {
-        next_state: u32,
-        agg_state: u32,
-    },
-    AggFlow {
-        next_state: u32,
-        reachable: HashSet<EntityId>,
     },
     InitFreq {
         next_state: u32,
@@ -48,19 +38,8 @@ impl TryFrom<StateInfo> for StateData {
 
     fn try_from(value: StateInfo) -> Result<Self, Self::Error> {
         match value {
-            StateInfo::Normal { next_state } => Ok(StateData::Normal { next_state }),
+            StateInfo::Default { next_state } => Ok(StateData::Default { next_state }),
             StateInfo::Output { subpattern_id } => Err(StateDataConstructionError::FromOutputState),
-            StateInfo::InitFlow {
-                next_state,
-                agg_state,
-            } => Ok(StateData::InitFlow {
-                next_state,
-                agg_state,
-            }),
-            StateInfo::AggFlow { next_state } => Ok(StateData::AggFlow {
-                next_state,
-                reachable: HashSet::new(),
-            }),
             StateInfo::InitFreq { next_state } => Ok(StateData::InitFreq { next_state }),
             StateInfo::AggFreq {
                 next_state,
