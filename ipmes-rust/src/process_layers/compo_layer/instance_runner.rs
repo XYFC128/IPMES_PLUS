d32999c6d848e2a6fd34f4c2ca8ba2371aa248c5
Daniel Liu <xyfc128@gmail.com>
2024-04-30 19:01:12 +0800

    test InstanceRunner::new

--- ipmes-rust/src/process_layers/compo_layer/instance_runner.rs
+++ ipmes-rust/src/process_layers/compo_layer/instance_runner.rs
@@ -13,7 +13,7 @@ use super::{InstanceStorage, MatchInstance, StateData, StateInfo};
 pub struct InstanceRunner<'p> {
     pub state_table: Vec<(StateInfo, FilterInfo)>,
     new_instance: Vec<(Filter, MatchInstance<'p>)>,
-    pub output_buffer: Vec<(u32, Box<[UniversalMatchEvent<'p>]>)>,
+    pub output_buffer: Vec<(u32, MatchInstance<'p>)>,
 }
 
 impl<'p> InstanceRunner<'p> {
@@ -82,13 +82,11 @@ impl<'p> InstanceRunner<'p> {
         } = instance.accept(match_event)
         {
             if let StateInfo::Output { subpattern_id } = self.state_table[new_state_id as usize].0 {
-                if instance.check_entity_uniqueness() {
-                    self.output_buffer
-                        .push(self.new_output_from(instance, new_event, subpattern_id));
+                if let Some(output) = self.new_output_from(&instance, new_event, subpattern_id) {
+                    self.output_buffer.push(output);
                 }
-            } else {
-                self.new_instance
-                    .push(self.new_instance_from(&instance, new_state_id, new_event));
+            } else if let Some(data) = self.new_instance_from(&instance, new_state_id, new_event) {
+                self.new_instance.push(data);
             }
         }
     }
@@ -98,13 +96,41 @@ impl<'p> InstanceRunner<'p> {
         instance: &MatchInstance<'p>,
         new_event: UniversalMatchEvent<'p>,
         subpattern_id: u32,
-    ) -> (u32, Box<[UniversalMatchEvent<'p>]>) {
+    ) -> Option<(u32, MatchInstance<'p>)> {
+        let old_filter = self.state_table[instance.state_id as usize].1;
+        let match_entities = match old_filter {
+            FilterInfo::Subject {
+                match_ord: _,
+                subject: _,
+            } => MatchInstance::dup_extend_entities_list(
+                &instance.match_entities,
+                new_event.subject_id,
+                new_event.matched.subject.id as u64,
+            )?,
+            FilterInfo::Object {
+                match_ord: _,
+                object: _,
+            } => MatchInstance::dup_extend_entities_list(
+                &instance.match_entities,
+                new_event.object_id,
+                new_event.matched.object.id as u64,
+            )?,
+            _ => instance.match_entities.clone(),
+        };
+
         let events = &instance.match_events;
         let mut new_match_events = Vec::with_capacity(events.len() + 1);
-        new_match_events.clone_from(events);
+        new_match_events.clone_from_slice(events);
         new_match_events.push(new_event);
 
-        (subpattern_id, new_match_events.into_boxed_slice())
+        Some((
+            subpattern_id,
+            MatchInstance {
+                match_events: new_match_events.into_boxed_slice(),
+                match_entities,
+                ..instance.clone()
+            },
+        ))
     }
 
     pub fn new_instance_from(
@@ -112,25 +138,50 @@ impl<'p> InstanceRunner<'p> {
         old_instance: &MatchInstance<'p>,
         new_state_id: u32,
         new_event: UniversalMatchEvent<'p>,
-    ) -> (Filter, MatchInstance<'p>) {
+    ) -> Option<(Filter, MatchInstance<'p>)> {
         let events = &old_instance.match_events;
 
         let (state_info, filter_info) = self.state_table[new_state_id as usize];
 
         let new_instance = match state_info {
-            StateInfo::Default { next_state: _ }
-            | StateInfo::InitFreq { next_state: _ } => {
-                let mut event_ids = old_instance.event_ids.clone();
+            StateInfo::Default { next_state: _ } | StateInfo::InitFreq { next_state: _ } => {
+                let mut event_ids = old_instance.event_ids.clone().into_vec();
                 event_ids.extend(new_event.event_ids.iter());
+                event_ids.sort();
+                let event_ids = event_ids.into_boxed_slice();
+
+                let old_filter = self.state_table[old_instance.state_id as usize].1;
+                let match_entities = match old_filter {
+                    FilterInfo::Subject {
+                        match_ord: _,
+                        subject: _,
+                    } => MatchInstance::dup_extend_entities_list(
+                        &old_instance.match_entities,
+                        new_event.subject_id,
+                        new_event.matched.subject.id as u64,
+                    )?,
+                    FilterInfo::Object {
+                        match_ord: _,
+                        object: _,
+                    } => MatchInstance::dup_extend_entities_list(
+                        &old_instance.match_entities,
+                        new_event.object_id,
+                        new_event.matched.object.id as u64,
+                    )?,
+                    _ => old_instance.match_entities.clone(),
+                };
 
                 let mut new_match_events = Vec::with_capacity(events.len() + 1);
-                new_match_events.clone_from(events);
+                new_match_events.clone_from_slice(events);
                 new_match_events.push(new_event);
+                let new_match_events = new_match_events.into_boxed_slice();
 
                 MatchInstance {
                     start_time: old_instance.start_time,
                     match_events: new_match_events,
+                    match_entities,
                     state_data: state_info.try_into().unwrap(),
+                    state_id: new_state_id,
                     event_ids,
                 }
             }
@@ -149,6 +200,7 @@ impl<'p> InstanceRunner<'p> {
                         frequency,
                         current_set,
                     },
+                    state_id: next_state,
                     ..old_instance.clone()
                 }
             }
@@ -182,11 +234,11 @@ impl<'p> InstanceRunner<'p> {
             },
         };
 
-        (filter, new_instance)
+        Some((filter, new_instance))
     }
 
     pub fn store_new_instances(&mut self, storage: &mut InstanceStorage<'p>) {
-        let new_instance = std::mem::replace(&mut self.new_instance, vec![]);
+        let new_instance = std::mem::take(&mut self.new_instance);
         for (filter, instance) in new_instance.into_iter() {
             match filter {
                 Filter::MatchOrdOnly { match_ord } => storage
@@ -220,18 +272,60 @@ impl<'p> InstanceRunner<'p> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{pattern::Pattern, sub_pattern::decompose};
+    use crate::pattern::Pattern;
 
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_new() {
         let pattern = Pattern::from_graph(
             &["v1", "v2", "v3", "v4"],
-            &[(0, 1, "e1"), (1, 2, "e2"), (1, 3, "e3")],
+            &[
+                (0, 1, "e1"), // share none
+                (1, 2, "e2"), // share subject
+                (3, 2, "e3"), // share object
+                (2, 3, "e4"), // share both
+            ],
             false,
         );
-        let decomposition = decompose(&pattern);
-        println!("{:#?}", decomposition);
+
+        let decomposition = [SubPattern {
+            id: 0,
+            events: pattern.events.iter().collect(),
+        }];
+
+        let ans = [
+            (
+                StateInfo::Default { next_state: 1 },
+                FilterInfo::MatchOrdOnly { match_ord: 0 },
+            ),
+            (
+                StateInfo::Default { next_state: 2 },
+                FilterInfo::Subject {
+                    match_ord: 1,
+                    subject: EntityEncode::object_of(0),
+                },
+            ),
+            (
+                StateInfo::Default { next_state: 3 },
+                FilterInfo::Object {
+                    match_ord: 2,
+                    object: EntityEncode::object_of(1),
+                },
+            ),
+            (
+                StateInfo::Default { next_state: 4 },
+                FilterInfo::Endpoints {
+                    match_ord: 3,
+                    // the shared subject could be object_of(1), but 2 is better
+                    // since it is closer
+                    subject: EntityEncode::object_of(2), 
+                    object: EntityEncode::subject_of(2),
+                },
+            ),
+            (StateInfo::Output { subpattern_id: 0 }, FilterInfo::None),
+        ];
+        let runner = InstanceRunner::new(&decomposition);
+        assert_eq!(runner.state_table, &ans)
     }
 }
