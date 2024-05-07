ac5c614589a72c5d3cfb3b2d955f9cd585705bc0
Daniel Liu <xyfc128@gmail.com>
2024-05-07 14:17:37 +0800

    update convert_data_graph

--- ipmes-rust/scripts/convert_data_graph.py
+++ ipmes-rust/scripts/convert_data_graph.py
@@ -1,4 +1,8 @@
 import fileinput
+import re
+import sys
+
+pattern = re.compile(r'(.+)#(\w+::.+)#(\w+::.+)')
 
 if __name__ == '__main__':
     for line in fileinput.input(inplace=True):
@@ -11,5 +15,16 @@ if __name__ == '__main__':
             continue
 
         t1, t2, sig, event_id, subject_id, object_id = columns
-        event_sig, subject_sig, object_sig = sig.split('#')
+        split = sig.split('#')
+        if len(split) == 3:
+            event_sig, subject_sig, object_sig = split
+        else:
+            found = pattern.search(sig)
+            if found is None:
+                print('Bad record:', line.strip(), file=sys.stderr)
+                continue
+            event_sig = found.group(1)
+            subject_sig = found.group(2)
+            object_sig = found.group(3)
+
         print(','.join([t1, t2, event_id, event_sig, subject_id, subject_sig, object_id, object_sig]), end='\n' if line.endswith('\n') else '')
